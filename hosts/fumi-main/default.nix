22e07cf1d95731740eb02a5d8fdb37f35fea282e
fumi <expugnatiomundi@gmail.com>
2026-04-12 22:12:47 +0900

    codec changes

--- hosts/fumi-main/default.nix
+++ hosts/fumi-main/default.nix
@@ -71,6 +71,19 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    alsa.enable = true;
+    wireplumber.enable = true;
+  };
+
+  # A2DP / AAC / LDACなどのコーデック対応
+  services.pipewire.wireplumber.extraConfig = {
+    "bluez-monitor.conf" = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+      };
+    };
   };
 
   fonts.packages = with pkgs; [
