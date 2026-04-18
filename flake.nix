fcde60887f09a752716034ac019fcc485708a09b
fumi <expugnatiomundi@gmail.com>
2026-04-18 17:14:41 +0900

    add vivaldi

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # 安定が良ければ nixos-25.11 みたいに固定してOK
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -20,7 +19,13 @@
         ./hosts/${host}/hardware-configuration.nix
         ./hosts/${host}/default.nix
 
-        # Home Manager を NixOS module として統合
+        ({ lib, ... }: {
+          nixpkgs.config.allowUnfreePredicate = pkg:
+            builtins.elem (lib.getName pkg) [
+              "vivaldi"
+            ];
+        })
+
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
@@ -31,26 +36,4 @@
       ];
     };
   };
-  # let
-  #   system = "x86_64-linux";
-  #   host = "fumi-no-nixos";
-  # in {
-  #   nixosConfigurations."fumi-no-nixos" = nixpkgs.lib.nixosSystem {
-  #     inherit system;
-
-  #     modules = [
-  #       ./hosts/${host}/hardware-configuration.nix
-  #       ./hosts/${host}/default.nix
-
-  #       # Home Manager を NixOS module として統合
-  #       home-manager.nixosModules.home-manager
-  #       {
-  #         home-manager.useGlobalPkgs = true;
-  #         home-manager.useUserPackages = true;
-  #         home-manager.backupFileExtension = "bak";
-  #         home-manager.users.fumi = import ./home/fumi.nix;
-  #       }
-  #     ];
-  #   };
-  # };
 }
