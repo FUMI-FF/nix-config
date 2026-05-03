2708bff3d8296a8da1f252d5b75aeabbfae1ee29
fumi <expugnatiomundi@gmail.com>
2026-05-03 21:32:44 +0900

    work around Realtek Wi-Fi PCIe init issue

--- hosts/fumi-main/default.nix
+++ hosts/fumi-main/default.nix
@@ -1,5 +1,17 @@
-{ hostName, ... }:
+{ hostName, pkgs, ... }:
 
 {
   networking.hostName = hostName;
+
+  boot.kernelParams = [
+    "pcie_aspm=off"
+  ];
+
+  services.fwupd.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    fwupd
+    pciutils
+    usbutils
+  ];
 }
